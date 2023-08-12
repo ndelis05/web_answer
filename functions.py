e02e3c89f19ed3c0ab72c37ec386f7f9a8fb1daf
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2023-08-12 17:26:53 -0500

    many updates

---
+++ functions.py
@@ -0,0 +1,15 @@
+function_descriptions = [   {
+        "name": "websearch",
+        "description": "Get the latest updates about this topic from the web",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "topic": {
+                    "type": "string",
+                    "description": "The topic to investigate"
+                },
+            },
+            "required": ["topic"]
+        }
+    },
+]
---
+++ prompts.py
@@ -0,0 +1,18 @@
+system_context = """You are an experienced physician and surgeon. You answer questions carefully and to the point using
+evidence based medicine to assist clinical learners, the only readers of your responses so no disclaimers needed.
+"""
+
+sample_question = "How should I treat localized cellulitis in an otherwise normal patient?"
+sample_response = """Localized cellulitis in an otherwise healthy patient is typically treated with antibiotics. The choice of antibiotic can depend on the suspected causative organism, usually Staphylococcus aureus or Streptococcus pyogenes.
+
+Here are some general steps for treating localized cellulitis:
+
+1. **Oral Antibiotics**: For a healthy patient with localized cellulitis, oral antibiotics are usually the first line of treatment. Commonly prescribed antibiotics include cephalexin or dicloxacillin. If MRSA is suspected, doxycycline or trimethoprim-sulfamethoxazole may be used.
+
+2. **Monitor the Patient**: It's important to monitor the patient's response to treatment. Improvement should be seen within a few days. If the cellulitis doesn't improve, the patient may need to be reevaluated and possibly switched to a different antibiotic.
+
+3. **Supportive Care**: Encourage the patient to rest and elevate the affected area to help reduce swelling. Over-the-counter pain relievers can also be used to manage discomfort.
+
+4. **Prevention**: Good skin hygiene can help prevent future episodes of cellulitis. This includes keeping the skin clean and moisturized, promptly treating any cuts or scrapes, and using a moisturizer to prevent skin cracking.
+
+Remember, this is a general guideline and the treatment may vary based on the patient's specific condition and response to medication. Always consult with a healthcare professional for the most appropriate treatment."""
