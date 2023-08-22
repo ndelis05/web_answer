607e892e68e6e1323298fd6717d69a78ec75e0dd
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2023-08-22 17:42:04 -0500

    one more fix

--- pages/😷_pt_sims.py
+++ pages/😷_pt_sims.py
@@ -230,8 +230,8 @@ if check_password2():
     if clear_memory:
         st.session_state.langchain_messages = []
         clear_session_state_except_password_correct()
-        if "last_response" not in st.session_state:
-            st.session_state["last_response"] = "Patient Response: I can't believe I'm in the Emergency Room feeling sick!"
+        # if "last_response" not in st.session_state:
+        #     st.session_state["last_response"] = "Patient Response: I can't believe I'm in the Emergency Room feeling sick!"
     # Audio response section 
     # Define the URL and headers
     audio_url = "https://play.ht/api/v2/tts"
