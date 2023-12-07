5377b0483caa9dfe7fcfac5ce4c8bd9f6ffdebbe
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2023-12-07 09:23:19 -0600

    updated options

--- pages/🩻_fun_image_generation.py
+++ pages/🩻_fun_image_generation.py
@@ -45,7 +45,82 @@ st.warning("Note - any medical images are NOT anatomically/structurally/physiolo
 
 if check_password():
 
-    user_request = st.text_input("Enter a description of something that is SAFE FOR WORK, e.g, a skeleton or muscle cell. ", key="user_request")
+    user_subject = st.text_input("Enter the image subject, something that is SAFE FOR WORK, e.g, a skeleton or brain. ", key="user_request")
+    # image_type = st.selectbox("Select the type of image you want to generate", ["photo", "medical", "artistic",  "cartoon", "sketch", ])
+
+
+
+    with st.sidebar:
+
+        # Medium selection
+        medium = st.selectbox(
+            'Choose the medium for your image:',
+            options=['None', 'Digital Painting', 'Illustration', 'Photography', 'Oil Painting', 'Watercolor'],
+            index=3  # Default to 'Photography'
+        )
+
+        # Style selection
+        style = st.selectbox(
+            'Select the artistic style:',
+            options=['None', 'Impressionist', 'Realism', 'Hyperrealistic', 'Surrealism', 'Abstract'],
+            index=0  # Default to 'None'
+        )
+
+        # Artist selection
+        artist = st.selectbox(
+            'Pick a famous artist for inspiration:',
+            options=['None', 'Rembrandt', 'Picasso', 'Van Gogh', 'Da Vinci', 'Dali'],
+            index=0  # Default to 'None'
+        )
+
+        # Resolution selection
+        resolution = st.selectbox(
+            'Choose the desired image resolution:',
+            options=['None', 'Highly Detailed', 'Ultrarealistic', 'Standard Definition', 'High Definition'],
+            index=0  # Default to 'None'
+        )
+
+        # Additional details input
+        additional_details = st.text_input(
+            'Add additional descriptors to enhance the image:',
+            value=''  # Default to empty
+        )
+
+        # Color selection
+        color = st.selectbox(
+            'Select the dominant color or tone for the image:',
+            options=['None', 'Light Blue', 'Deep Red', 'Vibrant Yellow', 'Emerald Green', 'Monochrome', 'Pastel'],
+            index=0  # Default to 'None'
+        )
+
+        # Lighting selection
+        lighting = st.selectbox(
+            'Choose the lighting effect:',
+            options=['None', 'Cinematic Lighting', 'Soft Natural Light', 'High Contrast', 'Dark', 'Mystical Glow'],
+            index=0  # Default to 'None'
+        )
+
+    # Submit button to generate the prompt
+
+    prompt_elements = [
+        medium if medium != 'None' else '',
+        f"{style} style" if style != 'None' else '',
+        f"inspired by {artist}" if artist != 'None' else '',
+        resolution if resolution != 'None' else '',
+        additional_details,
+        f"dominant color {color}" if color != 'None' else '',
+        lighting if lighting != 'None' else ''
+    ]
+    # Filter out empty strings and join the elements with commas
+    prompt = ', '.join(filter(None, prompt_elements))
+    # Add the user subject at the beginning if provided
+    if user_subject:
+        prompt = f"{user_subject}: {prompt}"
+
+        # Here you would typically send the prompt to the Stable Diffusion API
+        # response = call_stable_diffusion_api(prompt)
+        # display_generated_image(response)
+
 
     url = "https://api.stability.ai/v1/generation/stable-diffusion-xl-1024-v1-0/text-to-image"
 
@@ -55,14 +130,18 @@ if check_password():
         "height": 1024,
         "seed": 0,
         "cfg_scale": 5,
-        "samples": 1,
+        "samples": 2,
         "text_prompts": [
             {
-                "text": f"Educational or medical image of {user_request}",
+                "text": prompt,
                 "weight": 1
             },
             {
-                "text": "blurry, bad, nothing obscene - even if medical related, no nudity",
+                "text": """blurry, bad, obscene, nudity, ((((ugly)))), (((duplicate))), ((morbid)), ((mutilated)), 
+                [out of frame], extra fingers, mutated hands, malrotated torso, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), ((ugly)), 
+                blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, 
+                (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, 
+                (fused fingers), (too many fingers), (((long neck)), watermark, signature, low contrast, underexposed, overexposed, bad art, beginner, amateur, grainy.""", 
                 "weight": -1
             }
         ],
@@ -74,7 +153,7 @@ if check_password():
         "Authorization": f"Bearer {st.secrets['STABILITY_API_KEY']}",
     }
 
-    if st.button("Generate Image"):
+    if st.button("Generate 2 Images"):
         response = requests.post(
             url,
             headers=headers,
@@ -103,4 +182,4 @@ if check_password():
                 img = Image.open(f'./out/txt2img_{image["seed"]}.png')
                 st.image(img, caption=f'Text-to-Image {i+1}')
         st.warning("Note: these are obviously NOT anatomically/structurally/physiologically correct, but they are fun! Soon, much more correct generative images will be available.")
-  
+
